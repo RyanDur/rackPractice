b7b911b10714cb4e685d5e90c0151d3849316902
Ryan Durling <ryan.p.durling@gmail.com>
2013-06-06 15:33:16 -0700

    add map to url for payloads

--- rackBuilderPractice.rb
+++ rackBuilderPractice.rb
@@ -3,7 +3,14 @@ require "rack"
 infinity = Proc.new {|env| [200, {"content-type" => "text/html"}, [env.inspect]]}
 builder = Rack::Builder.new do
   use Rack::CommonLogger
-  run infinity
+
+  map "/" do
+    run infinity
+  end
+
+  map "/version" do
+    run Proc.new {|env| [200, {"content-type" => "text/html"}, ["infinity 0.1"]]}
+  end
 end
 
 Rack::Handler::Thin.run builder, :Port => 9292

5c92e0b3e6eb94a85bee5d50d2bc5f15ea779ce3
Ryan Durling <ryan.p.durling@gmail.com>
2013-06-06 00:38:55 -0700

    Removed class and replaced with a onliner with a proc

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -1,11 +1,15 @@
 require "rubygems"
 require "rack"
 
-class HelloWorld
-  def call(env)
-    puts env.each{|x| puts x.inspect}
-    [200, {"content-type" => "text/html"}, "Hello Rack"]
-  end
-end
 
-Rack::Handler::Thin.run HelloWorld.new, :Port => 9292
+#class HelloWorld
+#  def call(env)
+#    puts env.each{|x| puts x.inspect}
+#    [200, {"content-type" => "text/html"}, "Hello Rack"]
+#  end
+#end
+
+#Rack::Handler::Thin.run HelloWorld.new, :Port => 9292
+
+
+Rack::Handler::Thin.run Proc.new{|env| [200, {"content-type" => "text/html"}, "Hello Rack, I'm a Proc"]}, :Port => 9292
